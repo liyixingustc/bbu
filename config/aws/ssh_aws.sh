26461735f935fd55c88490eaa28e38a82f7dd4e1
arthurtu <sihan.tu@uconn.edu>
2017-07-12 15:12:56 -0400

    update website

--- config/aws/ssh_aws.sh
+++ config/aws/ssh_aws.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 echo -e "+++++++++++++++++++++++++++++++++++++++++++"
-echo -e "+++ Connecting to di-dedicated server... ++"
+echo -e "+++ Connecting to tlinvestment server... ++"
 echo -e "+++++++++++++++++++++++++++++++++++++++++++"
 
-ssh -i /home/arthurtu/projects/bbu/config/aws/tlinvestment.pem ec2-user@54.236.23.211
+ssh -i /home/arthurtu/projects/bbu/config/aws/tlinvestment.pem ec2-user@54.86.139.19
