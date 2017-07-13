cd841bfa2b0520b8edb7223155aee2932ed664e4
arthurtu <sihan.tu@uconn.edu>
2017-07-12 23:41:08 -0400

    update website

--- config/aws/ssh_aws.sh
+++ config/aws/ssh_aws.sh
@@ -5,4 +5,4 @@ echo -e "+++++++++++++++++++++++++++++++++++++++++++"
 echo -e "+++ Connecting to tlinvestment server... ++"
 echo -e "+++++++++++++++++++++++++++++++++++++++++++"
 
-ssh -i /home/arthurtu/projects/bbu/config/aws/tlinvestment.pem ec2-user@54.86.139.19
+ssh -i /home/arthurtu/projects/bbu/config/aws/tlinvestment.pem ec2-user@204.236.208.173
