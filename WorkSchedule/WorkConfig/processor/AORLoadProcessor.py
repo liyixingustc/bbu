1c2a0c77c1ce6650ace88f2fe64a5c3880f40dc4
arthurtu <sihan.tu@uconn.edu>
2017-08-25 15:23:34 -0400

    update website

---
+++ WorkSchedule/WorkConfig/processor/AORLoadProcessor.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import pandas as pd
+import re
+from datetime import datetime as dt
+import pytz
+import time
+
+from bbu.settings import TIME_ZONE, BASE_DIR
+from django.http import JsonResponse
+
+from ...WorkConfig.models.models import *
+from ...WorkWorkers.models.models import *
+from ...WorkTasks.models.models import *
+
+from utils.UDatetime import UDatetime
+
+EST = pytz.timezone(TIME_ZONE)
+
+
+class AORLoadProcessor:
+    pass
