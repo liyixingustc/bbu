2f8f579b0371ef493392d242f62dbd3e2a1ad8a3
arthurtu <sihan.tu@uconn.edu>
2017-09-21 13:56:56 -0400

    update website

--- bbu/settings.py
+++ bbu/settings.py
@@ -173,3 +173,10 @@ REST_FRAMEWORK = {
 
 # Celery settings
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+#: Only add pickle to this list if your broker is secured
+#: from unwanted access (see userguide/security.html)
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_RESULT_BACKEND = 'db+sqlite:///db.sqlite3'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
