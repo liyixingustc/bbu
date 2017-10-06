c30825472a699d688a5e7fabb1684a7ea99a27c1
arthurtu <sihan.tu@uconn.edu>
2017-10-06 15:56:50 -0400

    update website

--- spider/somax/somax_spider.py
+++ spider/somax/somax_spider.py
@@ -50,6 +50,7 @@ class SomaxSpider:
 
     somax_logo_id = 'imgLogo'
     somax_page_title_id = 'ctlPageDetails_lblPageTitle'
+    somax_table_export_div_id = 'MainContent_uicSearchHeader_dxBtnExport'
     somax_task_filter_work_order_id = 'MainContent_grdResults_DXFREditorcol0_I'
     somax_task_first_row_work_order_a_id = 'MainContent_grdResults_cell0_0_ColClientLookUpId_0'
     somax_task_first_row_work_order_td_id = 'MainContent_grdResults_tccell0_0'
@@ -132,7 +133,10 @@ class SomaxSpider:
             self.get_and_ready(self.somax_equipment_url)
             print(2)
             print(self.driver.page_source)
-            self.driver.find_element_by_id('MainContent_uicSearchHeader_dxBtnExport').click()
+            element_export = WebDriverWait(self.driver, 60).until(
+                EC.element_to_be_clickable((By.ID, self.somax_table_export_div_id)))
+            element_export.click()
+            # self.driver.find_element_by_id('MainContent_uicSearchHeader_dxBtnExport').click()
             print(3)
             after = os.listdir(self.download_path)
             self.driver.execute_script("window.stop();")
