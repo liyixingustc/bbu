e8fbee931012e2eae05bb3cd885a53203b9ea812
arthurtu <sihan.tu@uconn.edu>
2017-10-10 14:26:09 -0400

    update website

--- bbu/celery.py
+++ bbu/celery.py
@@ -5,6 +5,7 @@ from celery.schedules import crontab
 # set the default Django settings module for the 'celery' program.
 import os
 import sys
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
@@ -17,6 +18,7 @@ app = Celery('bbu')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.conf.timezone = 'America/New_York'
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
@@ -26,20 +28,22 @@ app.autodiscover_tasks()
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
+
+# time in UTC
 app.conf.beat_schedule = {
     'sync_equipment': {
         'task': 'spider.somax.tasks.sync_equipment',
-        'schedule': crontab(minute=0, hour='*/3'),
+        'schedule': crontab(minute=0, hour=0),
         'args': (),
     },
     'sync_pm': {
         'task': 'spider.somax.tasks.sync_pm',
-        'schedule': crontab(minute=10, hour='*/3'),
+        'schedule': crontab(minute=0, hour=2),
         'args': (),
     },
     'sync_task': {
         'task': 'spider.somax.tasks.sync_task',
-        'schedule': crontab(minute=30, hour='*/3'),
+        'schedule': crontab(minute=0, hour=3),
         'args': (),
     },
 }
